b9a8f091b1a2713b5afc18cd63ac06baf63a5212
James Sidhu <james.thomas.sidhu@gmail.com>
2017-01-11 16:46:51 +0000

    fix server for schema and set options for now

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 var express = require('express');
 var postgraphql = require('postgraphql').postgraphql;
 
-const SCHEMA = process.env.PGQLSCHEMA;
+const SCHEMA = process.env.PGQL_SCHEMA;
 const CONNECTION = process.env.PGQL_CONNECTION;
 
 const OPTIONS = {
-  graphiql: process.env.PGQL_GRAPHIQL || true, // @todo set to true
-  watchPg: true
+  graphiql: process.env.PGQL_GRAPHIQL || true,
 };
 
 const app = express();

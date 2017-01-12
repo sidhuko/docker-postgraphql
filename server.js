444f4225e45b526a5a7e7d28b79bf7fc5f3f39bd
James Sidhu <james.thomas.sidhu@gmail.com>
2017-01-12 11:21:58 +0000

    handle min library functionality

--- server.js
+++ server.js
@@ -3,14 +3,48 @@ var postgraphql = require('postgraphql').postgraphql;
 
 const SCHEMA = process.env.PGQL_SCHEMA;
 const CONNECTION = process.env.PGQL_CONNECTION;
-
-const OPTIONS = {
-  graphiql: process.env.PGQL_GRAPHIQL || true,
-};
+const JWT_TOKEN = process.env.PGQL_JWT_TOKEN || false;
+const DEFAULT_ROLE = process.env.PGQL_DEFAULT_ROLE || false;
+const GRAPHIQL = process.env.PGQL_GRAPHIQL || false;
+const JWT_SECRET = process.env.PGQL_JWT_SECRET || false;
+const WATCH = process.env.PGQL_WATCH || false;
 
 const app = express();
 
-app.use(postgraphql(CONNECTION, SCHEMA, OPTIONS));
+const options = {
+  watchPg: process.env.PGQL_WATCH || false,
+};
+
+if (!SCHEMA) {
+  throw Error("Must be supplied a schema");
+}
+
+if (!CONNECTION) {
+  throw Error("Must be supplied a pg connection");
+}
+
+if (WATCH) {
+  options.watchPg = true;
+}
+
+if (JWT_SECRET) {
+  options.jwtSecret = true;
+}
+
+if (GRAPHIQL) {
+  options.graphiql = true;
+  options.graphiqlRoute = GRAPHIQL;
+}
+
+if (JWT_TOKEN) {
+  options.jwtPgTypeIdentifier = JWT_TOKEN;
+}
+
+if (DEFAULT_ROLE) {
+  options.pgDefaultRole = DEFAULT_ROLE;
+}
+
+app.use(postgraphql(CONNECTION, SCHEMA.split(','), options));
 
 app.listen(5000, () => {
   console.log('Server is listening');
